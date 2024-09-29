e5240a9859ee2c3a6fed9849dc4db10efdec91ca
Alex Milenkov <alexmilenkov07@gmail.com>
2024-09-29 11:42:27 +0300

    model test

--- petsagram_2024/petstagram_2024/pets/models.py
+++ petsagram_2024/petstagram_2024/pets/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+        blank=False,
+        null=False
+    )
+
+    personal_photo = models.URLField(
+        blank=False,
+        null=False
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f'{self.name}-{self.id}')
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name}'
--- petsagram_2024/petstagram_2024/settings.py
+++ petsagram_2024/petstagram_2024/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d+3a_cl_v1u$2o!t$4y)f5c9hmisoe3@^d!@krg)y_-f=it2jt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'petstagram_2024.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram_db",
+        "USER": "postgres",
+        "PASSWORD": "999999991",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
@@ -126,7 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static',  # Or any custom folder where your static files are stored
 ]
 
 # Default primary key field type
