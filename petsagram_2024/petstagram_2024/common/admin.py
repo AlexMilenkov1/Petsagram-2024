5386a094287b6e3c1a0a41c04f642326f91b51e3
Alex Milenkov <alexmilenkov07@gmail.com>
2024-10-04 00:45:37 +0300

    update admin.py

--- petsagram_2024/petstagram_2024/common/admin.py
+++ petsagram_2024/petstagram_2024/common/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from petstagram_2024.common.models import Comment, Like
+
+
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
