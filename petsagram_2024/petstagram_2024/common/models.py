985bae5cf543c5fcaef7b800741b7b097e428f9d
Alex Milenkov <alexmilenkov07@gmail.com>
2024-10-03 22:55:59 +0300

    made 2 models

--- petsagram_2024/petstagram_2024/common/models.py
+++ petsagram_2024/petstagram_2024/common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from petstagram_2024.photos.models import Photo
+
+
 # Create your models here.
+class Comment(models.Model):
+    text = models.TextField(max_length=300)
+    date_time_of_publication = models.DateTimeField(auto_now_add=True)
+    to_photo = models.ForeignKey(Photo, related_name='comments', on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(Photo, related_name='likes', on_delete=models.CASCADE)
