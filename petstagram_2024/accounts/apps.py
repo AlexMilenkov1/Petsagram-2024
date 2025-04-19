041106a43adf5111e88faf71eed4264bb1775304
Alex Milenkov <alexmilenkov07@gmail.com>
2025-04-19 13:07:13 +0300

    removed mailjet functionallity

--- petstagram_2024/accounts/apps.py
+++ petstagram_2024/accounts/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'petstagram_2024.accounts'
-
-    def ready(self):
-        import petstagram_2024.accounts.signals
--- petstagram_2024/settings.py
+++ petstagram_2024/settings.py
@@ -108,11 +108,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.AppUser'
 LOGIN_REDIRECT_URL = reverse_lazy('home-page')
 LOGOUT_REDIRECT_URL = reverse_lazy('home-page')
-
-# Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.example.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
